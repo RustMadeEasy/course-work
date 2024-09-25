6704f1db75c57721b21ec695e30c925e1e3b7f8e
RustMadeEasy <163663998+RustMadeEasy@users.noreply.github.com>
2024-09-25 00:37:10 -0700

    Refactoring

--- TicTacToe/tic-tac-toe-service/src/game_state.rs
+++ TicTacToe/tic-tac-toe-service/src/game_state.rs
@@ -5,11 +5,9 @@
  * @author JoelDavisEngineering@Gmail.com
  */
 
-use std::marker::PhantomData;
-
-
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
+
 
 use crate::errors::GameError;
 use crate::errors::GameError::{
@@ -26,153 +24,26 @@ use crate::models::PlayerInfo;
 use crate::play_outcome::PlayOutcome;
 use crate::play_status::PlayStatus;
 
-/// Models the state of a game at a particular move.
+/// Models the state of a game at a particular Move.
 #[derive(Clone, Default, Deserialize, Serialize, ToSchema)]
 pub(crate) struct GameState {
     /// ID of the Player who made this Move.
     id_of_player_who_made_move: String,
     pub(crate) game_board: [[GamePiece; MAX_BOARD_ROWS]; MAX_BOARD_COLUMNS],
-    #[serde(skip)]
-    _outside_instantiation_preventor: PhantomData<u8>,
     pub(crate) play_status: PlayStatus,
     pub(crate) winning_locations: Option<Vec<BoardPosition>>,
     pub(crate) winning_player_id: Option<String>,
 }
 
+// Initialization
 impl GameState {
     //
 
-    /// Generates binary representations of piece placements on a Game Board.
-    ///
-    /// Returns a tuple wherein the first element is the binary representation of placements for
-    /// game_piece_one and the second element is the binary representation of placements for
-    /// game_piece_two.
-    pub(crate) fn binary_representation_for_piece_placement(
-        grid: &GameBoard,
-        game_piece_one: &GamePiece,
-        game_piece_two: &GamePiece,
-    ) -> (i16, i16) {
-        //
-
-        if grid.is_empty() {
-            return (0, 0);
-        }
-
-        let mut position: i16 = 0b_100_000_000;
-        let mut binary_representation_one: i16 = 0;
-        let mut binary_representation_two: i16 = 0;
-
-        for row in grid {
-            for game_piece in row {
-                if game_piece == game_piece_one {
-                    binary_representation_one |= position;
-                } else if game_piece == game_piece_two {
-                    binary_representation_two |= position;
-                }
-                position >>= 1;
-            }
-        }
-
-        (binary_representation_one, binary_representation_two)
-    }
-
-    /// Determines the status of the GameBoard relative to a particular Game Piece. Returns the
-    /// status and, if the game has been won, returns the board positions that indicate the winning
-    /// move.
-    ///
-    /// Returns PlayOutcome.
-    fn determine_outcome_of_play(
-        grid: &GameBoard,
-        current_player_id: &str,
-        current_player_game_piece: &GamePiece,
-        other_player_game_piece: &GamePiece,
-    ) -> PlayOutcome {
-        //
-
-        let as_binary = Self::binary_representation_for_piece_placement(
-            grid,
-            current_player_game_piece,
-            other_player_game_piece,
-        );
-
-        // If there are no spaces filled, then the game has not started
-        if as_binary.0 == 0 && as_binary.1 == 0 {
-            return PlayOutcome::new(&PlayStatus::NotStarted);
-        }
-
-        // NOTE: The order in which we check the states is important!
-        let current_player_binary_representation = as_binary.0;
-
-        // Test for winning states
-        if (current_player_binary_representation & BIN_THREE_ACROSS_HORIZONTAL_TOP)
-            == BIN_THREE_ACROSS_HORIZONTAL_TOP
-            || (current_player_binary_representation & BIN_THREE_ACROSS_HORIZONTAL_MIDDLE)
-            == BIN_THREE_ACROSS_HORIZONTAL_MIDDLE
-            || (current_player_binary_representation & BIN_THREE_ACROSS_HORIZONTAL_BOTTOM)
-            == BIN_THREE_ACROSS_HORIZONTAL_BOTTOM
-            || (current_player_binary_representation & BIN_THREE_ACROSS_VERTICAL_LEFT)
-            == BIN_THREE_ACROSS_VERTICAL_LEFT
-            || (current_player_binary_representation & BIN_THREE_ACROSS_VERTICAL_CENTER)
-            == BIN_THREE_ACROSS_VERTICAL_CENTER
-            || (current_player_binary_representation & BIN_THREE_ACROSS_VERTICAL_RIGHT)
-            == BIN_THREE_ACROSS_VERTICAL_RIGHT
-            || (current_player_binary_representation & BIN_THREE_ACROSS_DIAGONAL_1)
-            == BIN_THREE_ACROSS_DIAGONAL_1
-            || (current_player_binary_representation & BIN_THREE_ACROSS_DIAGONAL_2)
-            == BIN_THREE_ACROSS_DIAGONAL_2
-        {
-            PlayOutcome::new_with_win_details(
-                &PlayStatus::EndedInWin,
-                &GameState::winning_board_positions_from_binary(current_player_binary_representation).unwrap(),
-                current_player_id,
-            )
-        } else if (as_binary.0 | as_binary.1) == BIN_FULL_BOARD {
-            PlayOutcome::new(&PlayStatus::EndedInStalemate)
-        } else {
-            PlayOutcome::new(&PlayStatus::InProgress)
-        }
-    }
-
-    /// Returns the ID of the player who the move at this Game State.
-    #[allow(dead_code)]
-    pub(crate) fn get_id_of_player_who_made_move(&self) -> String {
-        self.id_of_player_who_made_move.clone()
-    }
-
-    /// Returns the Game Board.
-    #[allow(dead_code)]
-    pub(crate) fn get_game_board(&self) -> GameBoard {
-        self.game_board.clone()
-    }
-
-    /// Returns the Play Status.
-    pub(crate) fn get_play_status(&self) -> PlayStatus {
-        self.play_status.clone()
-    }
-
-    /// Determines whether the Play Status indicates the end of the game.
-    pub(crate) fn has_ended(&self) -> bool {
-        match self.play_status {
-            PlayStatus::EndedInStalemate | PlayStatus::EndedInWin => true,
-            PlayStatus::InProgress | PlayStatus::NotStarted => false
-        }
-    }
-
-    /// Determines whether the specified position is a valid for the Tic-Tac_Toe game board.
-    pub(crate) fn is_valid_board_position(position: &BoardPosition) -> bool {
-        if position.column > (MAX_BOARD_COLUMNS - 1) || position.row > (MAX_BOARD_ROWS - 1) {
-            return false;
-        }
-        true
-    }
-
     /// Creates a new GameState instance.
-    #[allow(dead_code)]
     pub(crate) fn new() -> Self {
         Self {
             id_of_player_who_made_move: "".to_string(),
             game_board: Default::default(),
-            _outside_instantiation_preventor: Default::default(),
             play_status: PlayStatus::NotStarted,
             winning_locations: None,
             winning_player_id: None,
@@ -187,12 +58,45 @@ impl GameState {
         Self {
             id_of_player_who_made_move: current_player_id.to_string(),
             game_board: Default::default(),
-            _outside_instantiation_preventor: Default::default(),
             play_status: play_status.clone(),
             winning_locations: None,
             winning_player_id: None,
         }
     }
+}
+
+// Property accessors.
+impl GameState {
+    //
+
+    /// Returns the ID of the player who made the move at this Game State.
+    #[allow(dead_code)]
+    pub(crate) fn get_id_of_player_who_made_move(&self) -> String {
+        self.id_of_player_who_made_move.clone()
+    }
+
+    /// Returns the Game Board.
+    pub(crate) fn get_game_board(&self) -> GameBoard {
+        self.game_board.clone()
+    }
+
+    /// Returns the Play Status.
+    pub(crate) fn get_play_status(&self) -> PlayStatus {
+        self.play_status.clone()
+    }
+
+    /// Determines whether the game has ended.
+    pub(crate) fn has_ended(&self) -> bool {
+        match self.play_status {
+            PlayStatus::EndedInStalemate | PlayStatus::EndedInWin => true,
+            PlayStatus::InProgress | PlayStatus::NotStarted => false
+        }
+    }
+}
+
+// Actions
+impl GameState {
+    //
 
     /// Places the Player's game piece at the specified board position.
     pub(crate) fn place_game_piece(
@@ -244,11 +148,116 @@ impl GameState {
         Ok(Self {
             id_of_player_who_made_move: current_player.player_id.clone(),
             game_board,
-            _outside_instantiation_preventor: Default::default(),
             play_status: outcome.play_status,
             winning_locations: outcome.winning_position,
             winning_player_id: outcome.winning_player_id,
         })
+    }
+}
+
+// Helper functions
+impl GameState {
+    //
+
+    /// Generates binary representations of piece placements on a Game Board.
+    ///
+    /// Returns a tuple whose first element is the binary representation of placements for
+    /// game_piece_one and the second element is the binary representation of placements for
+    /// game_piece_two.
+    pub(crate) fn binary_representation_for_piece_placement(
+        grid: &GameBoard,
+        game_piece_one: &GamePiece,
+        game_piece_two: &GamePiece,
+    ) -> (i16, i16) {
+        //
+
+        if grid.is_empty() {
+            return (0, 0);
+        }
+
+        let mut position: i16 = 0b_100_000_000;
+        let mut binary_representation_one: i16 = 0;
+        let mut binary_representation_two: i16 = 0;
+
+        for row in grid {
+            for game_piece in row {
+                if game_piece == game_piece_one {
+                    binary_representation_one |= position;
+                } else if game_piece == game_piece_two {
+                    binary_representation_two |= position;
+                }
+                position >>= 1;
+            }
+        }
+
+        (binary_representation_one, binary_representation_two)
+    }
+
+    /// Determines the status of the GameBoard relative to a particular Game Piece. Returns the
+    /// status and, if the game has been won, returns the board positions that indicate the winning
+    /// move.
+    ///
+    /// Returns PlayOutcome.
+    fn determine_outcome_of_play(
+        game_board: &GameBoard,
+        current_player_id: &str,
+        current_player_game_piece: &GamePiece,
+        other_player_game_piece: &GamePiece,
+    ) -> PlayOutcome {
+        //
+
+        let as_binary = Self::binary_representation_for_piece_placement(
+            game_board,
+            current_player_game_piece,
+            other_player_game_piece,
+        );
+
+        // If there are no spaces filled, then the game has not started.
+        if as_binary.0 == 0 && as_binary.1 == 0 {
+            return PlayOutcome::new(&PlayStatus::NotStarted);
+        }
+
+        let current_player_binary_representation = as_binary.0;
+
+        // *** Test for winning states ***
+
+        // NOTE: The order in which we check the states is important!
+
+        if (current_player_binary_representation & BIN_THREE_ACROSS_HORIZONTAL_TOP)
+            == BIN_THREE_ACROSS_HORIZONTAL_TOP
+            || (current_player_binary_representation & BIN_THREE_ACROSS_HORIZONTAL_MIDDLE)
+            == BIN_THREE_ACROSS_HORIZONTAL_MIDDLE
+            || (current_player_binary_representation & BIN_THREE_ACROSS_HORIZONTAL_BOTTOM)
+            == BIN_THREE_ACROSS_HORIZONTAL_BOTTOM
+            || (current_player_binary_representation & BIN_THREE_ACROSS_VERTICAL_LEFT)
+            == BIN_THREE_ACROSS_VERTICAL_LEFT
+            || (current_player_binary_representation & BIN_THREE_ACROSS_VERTICAL_CENTER)
+            == BIN_THREE_ACROSS_VERTICAL_CENTER
+            || (current_player_binary_representation & BIN_THREE_ACROSS_VERTICAL_RIGHT)
+            == BIN_THREE_ACROSS_VERTICAL_RIGHT
+            || (current_player_binary_representation & BIN_THREE_ACROSS_DIAGONAL_1)
+            == BIN_THREE_ACROSS_DIAGONAL_1
+            || (current_player_binary_representation & BIN_THREE_ACROSS_DIAGONAL_2)
+            == BIN_THREE_ACROSS_DIAGONAL_2
+        {
+            PlayOutcome::new_with_win_details(
+                &PlayStatus::EndedInWin,
+                &GameState::winning_board_positions_from_binary(current_player_binary_representation).unwrap(),
+                current_player_id,
+            )
+        } else if (as_binary.0 | as_binary.1) == BIN_FULL_BOARD {
+            PlayOutcome::new(&PlayStatus::EndedInStalemate)
+        } else {
+            PlayOutcome::new(&PlayStatus::InProgress)
+        }
+    }
+
+    /// Determines whether the specified position is a valid for the Tic-Tac-Toe game board.
+    pub(crate) fn is_valid_board_position(position: &BoardPosition) -> bool {
+        if position.column > (MAX_BOARD_COLUMNS - 1) || position.row > (MAX_BOARD_ROWS - 1) {
+            return false;
+        }
+        true
     }
 
     /// If the specified binary representation connotes a winning board layout, this method converts
